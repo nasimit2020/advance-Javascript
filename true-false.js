c35304c63faef9bed29a3f9bf4d0663800af9ea7
nasimit2020 <nasimitjesbd@gmail.com>
2020-10-30 14:27:22 +0600

    true-false javascript

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// falsy;
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+
+// Truthy;
+// "0"
+// " "
+// arrary []
+// object {}
+// "false"
+
+
+
+const name = "Soliman Khan";
+
+if (name) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
