c2744f753e3a0636508c056664d895e0cb79adef
nasimit2020 <nasimitjesbd@gmail.com>
2020-10-30 20:43:26 +0600

    null vs undefined complete for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,12 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(13, 21);
+console.log('add :', result);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
